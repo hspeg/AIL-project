18c971b70718e7ebda7d482d5a22522b5998db2c
Vili05 <104265232+Vili05@users.noreply.github.com>
2023-01-01 19:13:27 +0100

    some new code

--- Utils.py
+++ Utils.py
@@ -9,19 +9,24 @@ from sklearn.metrics import accuracy_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import classification_report
 from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import GridSearchCV
 
 def evaluation_metrics(model, x_train, x_test, y_train, y_test):
     # prediction:
     y_pred_train = model.predict(x_train)
     y_pred_test = model.predict(x_test)
     y_pred_proba_test = model.predict_proba(x_test)
+    # accuracy and sensitivity
+    accuracy = accuracy_score(y_test, y_pred_test)
+    sensitivity = recall_score(y_test, y_pred_test)
     # classification reports:
     print("TRAIN SET REPORT:")
     names = ["surv. status >= 5 years", "surv. status < 5 years"]
     print(classification_report(y_train, y_pred_train, target_names=names))
     #
-    print("TEST SET REPORT:")
-    print(classification_report(y_test, y_pred_test, target_names=names))
+    print("TEST SET REPORT (PREDICTION):")
+    c_report_pred = classification_report(y_test, y_pred_test, target_names=names)
+    print(c_report_pred)
     # ROC curve and AUC
     fpr, tpr, treshold = roc_curve(y_test, y_pred_proba_test[:,1])
     auc_m = auc(fpr, tpr)
@@ -33,5 +38,19 @@ def evaluation_metrics(model, x_train, x_test, y_train, y_test):
     plt.title("ROC")
     plt.xlabel("False positive rate"), plt.ylabel("True positive rate")
     plt.show() 
+    
+    return c_report_pred, auc_m
 
-
+def cv_scores(model, param_grid, score, x_train, y_train):
+    # cross-validation
+    grid = GridSearchCV(model, param_grid, scoring=score, cv=5)
+    grid.fit(x_train, y_train)
+    # plotting the scores
+    scores = np.array(grid.cv_results_["mean_test_score"])
+    param = np.array([value for value in param_grid.values()])[0]
+    param_name = [key for key in param_grid.keys()][0]
+    plt.figure()
+    plt.plot(param, scores, "o-")
+    plt.xlabel(param_name); plt.ylabel(score)
+    plt.title("Cross-validation score")
+    plt.show()
